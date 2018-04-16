6900814415f717c09b01894ce1335402dc770c1f
smotouchi <shuya-motouchi@gmo.jp>
2018-04-16 17:22:02 +0900

    まだクソザコ

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,32 @@ static PANGRAM: &'static str =
 fn main() {
     let args: Vec<String> = env::args().collect();
     let checkfile = &args[1];
-    let process = match Command::new("wc")
+    let remotefile = &args[2];
+    let ruser = &args[3];
+    let rserver = &args[4];
+    let ssh_file = format!("(ssh {}@{} 'cat {}')",ruser,rserver,remotefile);
+    let process = match Command::new("diff")
+                                .arg("<")
+                                .arg(ssh_file)
                                 .arg(checkfile)
+                                //.arg(remotefile)
                                 .stdin(Stdio::piped())
                                 .stdout(Stdio::piped())
                                 .spawn() {
-        Err(why) => panic!("couldn't spawn wc: {}", Error::description(&why)),
+        Err(why) => panic!("couldn't spawn diff: {}", Error::description(&why)),
         Ok(process) => process,
     };
 
     match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
-        Err(why) => panic!("couldn't write to wc stdin: {}",
+        Err(why) => panic!("couldn't write to diff stdin: {}",
                            Error::description(&why)),
-        Ok(_) => println!("sent pangram to wc"),
+        Ok(_) => println!("sent pangram to diff"),
     }
 
     let mut s = String::new();
     match process.stdout.unwrap().read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read wc stdout: {}",
+        Err(why) => panic!("couldn't read diff stdout: {}",
                            Error::description(&why)),
-        Ok(_) => print!("wc responded with:\n{}", s),
+        Ok(_) => print!("diff responded with:\n{}", s),
     }
 }
