ef588c74d27b8e33dbeccfcab7ca3cf6f008ad53
smotouchi <shuya-motouchi@gmo.jp>
2018-04-16 16:41:39 +0900

    適当にどんどん強くする

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::process::Command;
+use std::error::Error;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+use std::env;
+
+static PANGRAM: &'static str =
+"the quick brown fox jumped over the lazy dog\n";
 
 fn main() {
-    let output = Command::new("rustc")
-        .arg("--version")
-        .output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-    });
+    let args: Vec<String> = env::args().collect();
+    let checkfile = &args[1];
+    let process = match Command::new("wc")
+                                .arg(checkfile)
+                                .stdin(Stdio::piped())
+                                .stdout(Stdio::piped())
+                                .spawn() {
+        Err(why) => panic!("couldn't spawn wc: {}", Error::description(&why)),
+        Ok(process) => process,
+    };
 
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout);
+    match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+        Err(why) => panic!("couldn't write to wc stdin: {}",
+                           Error::description(&why)),
+        Ok(_) => println!("sent pangram to wc"),
+    }
 
-        print!("rustc succeeded and stdout was:\n{}", s);
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr);
-
-        print!("rustc failed and stderr was:\n{}", s);
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read wc stdout: {}",
+                           Error::description(&why)),
+        Ok(_) => print!("wc responded with:\n{}", s),
     }
 }
